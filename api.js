18e94162cc89575319a8a023c4450d4c23343708
dyzz2025 <dyzz20233@protonmail.com>
2024-12-07 23:23:33 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,10 +1,10 @@
 window.onload = function() {
-    // 从指定的URL获取新的域名
+    
     fetch('https://u001.cyou/url.php')
-        .then(response => response.text()) // 假设响应是纯文本
+        .then(response => response.text()) 
         .then(domain => {
-            const nextUrl = 'https://' + domain.trim(); // 构建完整的URL
-            window.location.replace(nextUrl); // 重定向到新的URL
+            const nextUrl = 'https://' + domain.trim(); 
+            window.location.replace(nextUrl); 
         })
         .catch(error => {
             console.error('获取URL时出错：', error);
